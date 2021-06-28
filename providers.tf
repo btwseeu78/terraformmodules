a112155b64ce0043bf05493e51900bda19bdb8c3
arpan <arpan.b.chatterjee@gmail.com>
2021-06-28 19:07:13 +0530

    provider_added

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("atomic-crowbar-313911-cce1580891be.json")
+  project     = "atomic-crowbar-313911"
+  region      = "us-central1"
+  zone        = "us-central-c"
+}
